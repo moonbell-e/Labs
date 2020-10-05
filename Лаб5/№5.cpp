edaaf95574851a89a171f0d516d96da3eb515281
aigerimalmazbekkyzy <71431806+aigerimalmazbekkyzy@users.noreply.github.com>
2020-10-05 15:21:46 +0300

    Update №5.cpp

--- Лаб5/№5.cpp
+++ Лаб5/№5.cpp
@@ -7,9 +7,9 @@ int main()
 	float x1, y1, x2, y2, x3, y3, a, b, c;
 	cout << "Введите координаты точек через пробел: ";
 	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-	a = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-	b = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
-	c = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2));
+	a = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	b = sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+	c = sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
 	cout << "Длины сторон треугольника равны:  " << "\na = " << a
 		<< "\nb = " << b
 		<< "\nc = " << c
