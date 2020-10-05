c3efe51761766abd04e9a8d6e5ce5acdc9093b89
aigerimalmazbekkyzy <71431806+aigerimalmazbekkyzy@users.noreply.github.com>
2020-10-05 21:26:34 +0300

    Add files via upload

---
+++ Лаб6/№7.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int A, oleg, oleg2;
+	cout << "Введите число A: ";
+	cin  >> A;
+	oleg = A * A;
+	oleg2 = oleg * oleg;
+	oleg = oleg2 * A;
+	oleg2 = oleg * oleg * oleg;
+	cout << "Число А в пятнадцатой степени равно: " << oleg2;
+}
